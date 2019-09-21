cea145f14e2a8da1b1ca9db2ec6bcd4de33b1e14
LucasCron <lucaswcron@gmail.com>
2019-09-21 12:08:13 -0500

    formatting changes

--- Riot.py
+++ Riot.py
@@ -2,16 +2,20 @@ import requests
 import json
 import os
 
+
 def format_summoner_name(summoner_name):
     return summoner_name.replace(" ", "%20")
+
 
 def jprint(obj):
     # create a formatted string of the Python JSON object
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
+
 def get_api_response_json(url, parameters):
     return requests.get(url, parameters).json()
+
 
 # This is just a script for now
 
@@ -24,15 +28,15 @@ formatted_summoner_name = format_summoner_name(SUMMONER)
 print("Formatted summoner name: {}".format(formatted_summoner_name))
 
 parameters = {
-    "api_key" : API_KEY
+    "api_key": API_KEY
 }
 
-summoner_by_name_url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}".format(formatted_summoner_name)
+summoner_by_name_url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}".format(
+    formatted_summoner_name)
 summoner_by_name_response = get_api_response_json(summoner_by_name_url, parameters)
 jprint(summoner_by_name_response)
 
 id = summoner_by_name_response['id']
-print(id)
 
 champion_masteries_by_summoner_url = "https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by" \
                                      "-summoner/{}".format(id)
