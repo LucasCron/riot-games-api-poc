453c67706214079b13c6a41a4b8cf6020a9e3c45
LucasCron <lucaswcron@gmail.com>
2019-09-21 16:36:58 -0500

    renames

--- src/api/champion_mastery_v4.py
+++ src/api/champion_mastery_v4.py
@@ -2,13 +2,13 @@ from src.api.base_api import get_from_riot_api
 from src.api.champion_v4 import get_summoner_by_name
 
 base_url = "https://na1.api.riotgames.com"
-by_summoner_url = "{}/lol/champion-mastery/v4/champion-masteries/by-summoner/{}"
+mastery_by_summoner_url = "{}/lol/champion-mastery/v4/champion-masteries/by-summoner/{}"
 scores_by_summoner_url = "{}/lol/champion-mastery/v4/scores/by-summoner/{}"
 
 
 def get_mastery_by_summoner(summoner_name):
     summoner_id = get_summoner_by_name(summoner_name).get('id')
-    url = by_summoner_url.format(base_url, summoner_id)
+    url = mastery_by_summoner_url.format(base_url, summoner_id)
     return get_from_riot_api(url)
 
 
--- src/api/champion_v4.py
+++ src/api/champion_v4.py
@@ -4,13 +4,13 @@ from src.util.riot_utils import RiotUtils
 riot_utils = RiotUtils()
 
 base_url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners"
-by_account_url = "{}/by-account/{}"
-by_name_url = "{}/by-name/{}"
-by_puuid_url = "{}/by-puuid/{}"
-by_id_url = "{}/{}"
+summoner_by_account_url = "{}/by-account/{}"
+summoner_by_name_url = "{}/by-name/{}"
+summoner_by_puuid_url = "{}/by-puuid/{}"
+summoner_by_id_url = "{}/{}"
 
 
 def get_summoner_by_name(summoner_name):
     formatted_summoner_name = riot_utils.format_summoner_name(summoner_name)
-    url = by_name_url.format(base_url, formatted_summoner_name)
+    url = summoner_by_name_url.format(base_url, formatted_summoner_name)
     return get_from_riot_api(url)
