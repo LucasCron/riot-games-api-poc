1f847653aee1a71d2e246a86e303fd7d186222ae
LucasCron <lucaswcron@gmail.com>
2019-09-21 14:39:45 -0500

    add name to level and master lookup

--- Riot.py
+++ Riot.py
@@ -17,10 +17,6 @@ def get_api_response_json(url, parameters):
     return requests.get(url, parameters).json()
 
 
-# This is just a script for now
-
-print("Riot API here we come!")
-
 API_KEY = os.environ['API_KEY']
 SUMMONER = "Lu the Dirty"
 
@@ -34,10 +30,27 @@ parameters = {
 summoner_by_name_url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}".format(
     formatted_summoner_name)
 summoner_by_name_response = get_api_response_json(summoner_by_name_url, parameters)
-jprint(summoner_by_name_response)
+# jprint(summoner_by_name_response)
 
 id = summoner_by_name_response['id']
 
 champion_masteries_by_summoner_url = "https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by" \
                                      "-summoner/{}".format(id)
 champion_masteries_by_summoner_response = get_api_response_json(champion_masteries_by_summoner_url, parameters)
+# jprint(champion_masteries_by_summoner_response)
+
+ddragon_response_url = "http://ddragon.leagueoflegends.com/cdn/9.18.1/data/en_US/champion.json"
+ddragon_response_json = get_api_response_json(ddragon_response_url, parameters)
+
+champ_key_to_name_dict = {}
+
+# Make map for key -> name
+for (name, key) in ddragon_response_json['data'].items():
+    champ_key_to_name_dict[str(key['key'])] = str(name)
+
+for item in champion_masteries_by_summoner_response:
+    champ_id = item['championId']
+    champ_name = champ_key_to_name_dict[str(champ_id)]
+    champ_level = item['championLevel']
+    champ_score = item['championPoints']
+    print("Champion: {}, Level: {}, Score: {}".format(champ_name, champ_level, champ_score))
